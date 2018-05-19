5710322797a0f91bd40d969d734802ca400ef3b5
Patryk Lisik <patrykkbkak@gmail.com>
2018-05-19 22:45:07 +0200

    generowanie nauczycieli

--- DataGenerator/generator.py
+++ DataGenerator/generator.py
@@ -1,8 +1,16 @@
-import random as r
+import random
+from random import randint
+import string
 
-roomNrs = [(str(pietro) + "0" + str(pokoj))
-           for pietro in range(1, 10)
-           for pokoj in range(10)]
+
+def password_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def random_with_N_digits(n):
+    range_start = 10**(n - 1)
+    range_end = (10**n) - 1
+    return randint(range_start, range_end)
 
 
 def toList(fileToUse):
@@ -13,5 +21,34 @@ def toList(fileToUse):
 meskie = toList("meskie.txt")
 zenskie = toList("zenskie.txt")
 nazwiska = toList("nazwiska.txt")
+przedmioty = toList("przedmioty.txt")
+imiona = meskie.extend(zenskie)
+print(przedmioty)
 
-print(r.choice(meskie))
+
+def genNauczyciele(howMany):
+    ans = []
+    imiona_ = [random.choice(meskie) for _ in range(howMany)]
+    nazwiska_ = [random.choice(nazwiska) for _ in range(howMany)]
+    telefony = [random_with_N_digits(9) for _ in range(howMany)]
+    Emaile = [i.lower() + n.lower() + "@gmail.com" for i,
+              n in zip(imiona_, nazwiska_)]
+    # W ostatecznej wersji można dać jakieś 1
+    CzyAdmin = [0 for _ in range(howMany)]
+    UserLogin = [i.lower() + "_" + n.lower() for i,
+                 n in zip(imiona_, nazwiska_)]
+    hasla = [password_generator() for _ in range(howMany)]
+    ids = list(range(howMany))
+    for id, imie, naz, tel, mail,  Czyadm, login, pas in zip(ids, imiona_,
+                                                             nazwiska_,
+                                                             telefony,
+                                                             Emaile,
+                                                             CzyAdmin,
+                                                             UserLogin,
+                                                             hasla):
+        ans.append(
+            "INSERT INTO Nauczyciel VALUES({},{},{},{},{},{},{},{});"
+            .format(id, imie, naz, tel, mail, Czyadm, login, pas)
+        )
+
+    return ans
